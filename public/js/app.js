5f86b6e54eda06e2d47b93a217297c74045a62c3
Kh4yA <87660873+Kh4yA@users.noreply.github.com>
2024-03-29 16:11:40 +0100

    introduction mode responsive smartphone

---
+++ public/js/app.js
@@ -0,0 +1,69 @@
+const swiper1 = new Swiper('.swiper1', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: `1`,
+    spaceBetween: 50,
+    breakpoints: {
+        579:{
+            slidesPerView: 3,
+            spaceBetween: 10,
+
+        }
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+  });
+
+
+const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: `1`,
+
+    breakpoints: {
+        579:{
+            slidesPerView: 4,
+            spaceBetween:20
+        }
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+  });
+const swiperTechnologie = new Swiper('.swiper-technologie', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: `1`,
+
+    breakpoints: {
+        579:{
+            slidesPerView: 4,
+        }
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+  });
+const swiperNumber = new Swiper('.swiper-number', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: `1`,
+
+    breakpoints: {
+        579:{
+            slidesPerView: 4,
+        }
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+  });
